cff8d6b7c550e3425b33b588593aa3b70b03cb10
Oliamster <oliamster@gmail.com>
2020-04-17 04:56:40 +0200

    Initial commit

---
+++ iSize/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  iSize
+//
+//  Created by Mc Oliamster on 17/04/2020.
+//  Copyright © 2020 Oliamster. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
